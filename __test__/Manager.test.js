8774cbed5342a4ef90bceb92c5952ebb89b699c3
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-10-11 17:24:49 -0500

    consolidated class file

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,1 +1,15 @@
-const Manager = require("../lib/Manager");
+const { Manager } = require("../lib/Employee");
+
+// Create test suite for Employee Class
+describe("Employee", () => {
+    describe("Intialize Employee class", () => {
+        it("Assign arguments to class properties and test if values are equal", () => {
+            // Arrange
+            const employee = new Manager(0, "Gladriel", "gladriel@middleEarth.com", 10)
+            const {id, name, email} = employee
+
+            // Assert
+            expect(id === employee.id && name === employee.name && email === employee.email)
+        })
+    })
+})
